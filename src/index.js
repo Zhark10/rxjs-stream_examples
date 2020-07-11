a26efba700099aad1eb79b13dd1b6b9bf5ea4c3d
Zhark10 <verygoodman18@yandex.ru>
2020-07-12 01:43:32 +0300

    clear method added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { fromEvent } from 'rxjs'
 import { map } from 'rxjs/operators'
 
+// initialize elements
 const canvas = document.querySelector('canvas')
+const canvasCtx = canvas.getContext('2d')
+const clearButton = document.querySelector('button')
 
+// creation of streams
+const rectangle$ = fromEvent(canvas, 'mousemove')
+const clear$ = fromEvent(clearButton, 'click')
+
+// rectangle methods
 const dataConverter = event => ({
     x: event.offsetX,
     y: event.offsetY,
@@ -13,6 +21,15 @@ const drawing = currentPosition => {
     currentPosition.ctx.fillRect(currentPosition.x, currentPosition.y, 5, 5)
 }
 
-fromEvent(canvas, 'mousemove')
+// clear methods
+
+
+// stream handlers
+rectangle$
     .pipe(map(dataConverter))
     .subscribe(drawing)
+
+clear$
+    .subscribe(()=> {
+        canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+    })
