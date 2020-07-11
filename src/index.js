3eca4cccfee0931adc3e43f33deb35d57860ef45
Zhark10 <verygoodman18@yandex.ru>
2020-07-12 02:23:32 +0300

    added new option

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import { fromEvent } from 'rxjs'
 import { map } from 'rxjs/operators'
 
+(()=>{
 // initialize elements
 const canvas = document.querySelector('canvas')
 const canvasCtx = canvas.getContext('2d')
-const clearButton = document.querySelector('button')
+const decrementButton = document.getElementById('decrement')
+const incrementButton = document.getElementById('increment')
+const clearButton = document.getElementById('clear')
+const count = document.getElementById('count')
+let brushWidth = 5;
+count.value = brushWidth;
 
 // creation of streams
 const rectangle$ = fromEvent(canvas, 'mousemove')
 const clear$ = fromEvent(clearButton, 'click')
+const decrement$ = fromEvent(decrementButton, 'click')
+const increment$ = fromEvent(incrementButton, 'click')
 
 // rectangle methods
 const dataConverter = event => ({
@@ -18,11 +26,13 @@ const dataConverter = event => ({
 })
 
 const drawing = currentPosition => {
-    currentPosition.ctx.fillRect(currentPosition.x, currentPosition.y, 5, 5)
+    currentPosition.ctx.fillRect(currentPosition.x, currentPosition.y, brushWidth, brushWidth)
 }
 
 // clear methods
-
+const clearCanvas = () => {
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+}
 
 // stream handlers
 rectangle$
@@ -30,6 +40,21 @@ rectangle$
     .subscribe(drawing)
 
 clear$
-    .subscribe(()=> {
-        canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+    .subscribe(clearCanvas)
+
+decrement$
+    .subscribe(()=>{
+        if (brushWidth > 1) {
+            brushWidth--
+            count.value = brushWidth
+        }
     })
+    
+    increment$
+    .subscribe(()=>{
+        if (brushWidth < 80) {
+            brushWidth++
+            count.value = brushWidth
+        }
+    })
+})()
